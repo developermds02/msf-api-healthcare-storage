34ffff85f2b5495504529d236e9172ee1d7cc077
unknown <programador02@medicossalud.com>
2023-10-24 08:47:04 -0500

    add controller save

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+require('dotenv').config()
 const express = require('express')
-
 const app = express()
 
+const routerSave = require('./routes/saveFile') 
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+const { PORT, NODE_ENV, PORT_DEV } = process.env
+const port = NODE_ENV === 'development' ? PORT_DEV : PORT
+
+app.use(routerSave)
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+module.exports = { app, server }
